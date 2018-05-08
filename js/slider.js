3274525fa8abb49d8dd63b8b457eb9f72badf8a5
Pulkit Mishra <pulkitmishra007@gmail.com>
2018-05-08 21:01:20 +0530

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,9 @@
-//For Event Slider
 "use strict";
+  document.addEventListener('DOMContentLoaded',  function(){
+      //For Event Slider
       function scrollSliderContent(amount) {
         document.getElementById("slider-content").scrollLeft += amount;
       }
-
       document.getElementById("right-slider-btn").onclick = function () { scrollSliderContent(200) };
       document.getElementById("left-slider-btn").onclick = function () { scrollSliderContent(-200) };
   });
