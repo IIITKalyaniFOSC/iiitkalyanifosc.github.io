9c0970b47d1e6b80f12b457c0590f60f1ce57b2d
Abhishek Tiwari <abhishek.tiwari3507@gmail.com>
2018-05-10 18:36:42 +0530

    NEW FEATURE: Sliding menu for mobile devices (#15)
    sliding menu for mobile devices

---
+++ js/mobile-style.js
@@ -0,0 +1,8 @@
+//For Responsive Navigation Bar.
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
