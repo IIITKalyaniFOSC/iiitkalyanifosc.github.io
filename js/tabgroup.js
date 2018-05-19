e5feeb406533efc86678cb2d0f07ef0b8ee19b1b
Abhishek Tiwari <abhishek.tiwari3507@gmail.com>
2018-05-19 17:35:00 +0530

    tab display removed for mobile screens + introduction finalised

--- js/tabgroup.js
+++ js/tabgroup.js
@@ -1,25 +1,32 @@
 "use strict";
-document.addEventListener('DOMContentLoaded',  function(){
-  document.getElementById('tab-group').className = 'ready';
-  var headerClass = 'tab-header',
-      contentClass = 'tab-content';
-  var tabHeaders = document.getElementsByClassName(headerClass);
-  for(var i=0; i<tabHeaders.length; i++){
-    tabHeaders[i].addEventListener('click', activateTab);
-  }
-  function activateTab(event){
-    var currId = this.id,  
-        contentId = currId.replace('header', 'content'); 
-    deactivateAllTabs();
-    document.getElementById(currId).className = headerClass +' active';
-    document.getElementById(contentId).className = contentClass +' active';
-  }
-  function deactivateAllTabs(){
-    var tabHeader = document.getElementsByClassName(headerClass),
-        tabContents = document.getElementsByClassName(contentClass);
-    for(var i=0;i<tabHeaders.length;i++){
-      tabHeader[i].className = headerClass;
-      tabContents[i].className = contentClass;
+    function tabActivator(){
+         var w = parseInt(document.body.clientWidth);
+        if ( w>=768){
+        document.getElementById('tab-group').className = 'ready';
+        var headerClass = 'tab-header',
+            contentClass = 'tab-content';
+        var tabHeaders = document.getElementsByClassName(headerClass);
+        for(var i=0; i<tabHeaders.length; i++){
+          tabHeaders[i].addEventListener('click', activateTab);
+        }
+        function activateTab(event){
+          var currId = this.id,  //i.e - tab-header-1
+              contentId = currId.replace('header', 'content');   //result: tab-content-1
+              deactivateAllTabs();
+              document.getElementById(currId).className = headerClass +' active';
+              document.getElementById(contentId).className = contentClass +' active';
+        }
+        function deactivateAllTabs(){
+          var tabHeader = document.getElementsByClassName(headerClass),
+              tabContents = document.getElementsByClassName(contentClass);
+          for(var i=0;i<tabHeaders.length;i++){
+            tabHeader[i].className = headerClass;
+            tabContents[i].className = contentClass;
+          }
+        }
+    }
+    else{
+      document.getElementById("tab-group").className = "resp-tab ";
+
     }
   }
-});
