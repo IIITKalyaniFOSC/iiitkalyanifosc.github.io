4535fe166b3d7673f3517d5c595828ee54f57b5e
Satyam Kumar <satyambscity332@gmail.com>
2018-05-21 23:17:57 +0530

    Make 404 page responsive

--- js/404.js
+++ js/404.js
@@ -1,18 +1,21 @@
-//For Error 404
+//For Error 404 Page
 function colorstring(thecolor,thetext)
     {
     return '<font style=\"color:'+thecolor+'\">'+thetext+'</font>';
     }
 
-window.addEventListener('load', function(){
-
-	var indexcolor = Math.floor(Math.random()*10);
-	var language = Math.floor(Math.random()*10);
-	
-    var language = {"cplusplus": colorstring("green","cout<<\"") + "The page you are trying to find does not exist on our website. You can try going to the homepage, view source on github, change the language of this message, or report a bug." +colorstring("green","\";")};
-  
+function changetext(){
+	var colour = ["green", "blue","cyan","orange","red"];
+	var message = "The page you are trying to find does not exist on our website. You can go to the homepage, view source on github, change the language of this message, or report a bug.";
+	var indexcolor = Math.floor(Math.random()*colour.length);
+	var language = [{"resultstring": colorstring(colour[indexcolor],"cout<<\"") + message +colorstring(colour[indexcolor],"\";")}
+				   ];
+	var indexlanguage = Math.floor(Math.random()*language.length);
     var maintext = document.getElementById("mainmessage");
+    maintext.innerHTML = language[indexlanguage].resultstring;
   
-    maintext.innerHTML = language.cplusplus;
-  
-});
+}
+
+window.onload = function() {
+	changetext();
+}
