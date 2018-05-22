6ece9c81fe2cb28751b14407fe11f7c06d36c0dc
Satyam Kumar <satyambscity332@gmail.com>
2018-05-22 09:51:04 +0530

    Fix links in 404.html

--- js/404.js
+++ js/404.js
@@ -8,7 +8,9 @@ function changetext(){
 	var colour = ["green", "blue","cyan","orange","red"];
 	var message = "The page you are trying to find does not exist on our website. You can go to the homepage, view source on github, change the language of this message, or report a bug.";
 	var indexcolor = Math.floor(Math.random()*colour.length);
-	var language = [{"resultstring": colorstring(colour[indexcolor],"cout<<\"") + message +colorstring(colour[indexcolor],"\";")}
+	var language = [{"resultstring": colorstring(colour[indexcolor],"cout<<\"") + message +colorstring(colour[indexcolor],"\";")},
+					{"resultstring": colorstring(colour[indexcolor],"System.out.println(\"") + message +colorstring(colour[indexcolor],"\");")},
+					{"resultstring": colorstring(colour[indexcolor],"echo \"") + message +colorstring(colour[indexcolor],"\"")}
 				   ];
 	var indexlanguage = Math.floor(Math.random()*language.length);
     var maintext = document.getElementById("mainmessage");
